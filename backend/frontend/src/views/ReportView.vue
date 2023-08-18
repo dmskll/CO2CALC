a566ad9146979f5144eb0995125402dea2db42db
Dani M <dani.molleja@hyds.es>
2023-08-18 15:18:04 +0200

    add use equations

--- backend/frontend/src/views/ReportView.vue
+++ backend/frontend/src/views/ReportView.vue
@@ -1,16 +1,28 @@
 <template>
   <p>Report page!</p>
   <p>Hello {{ store.user_info.username }}</p>
-  FABRICACION
-  <div v-katex="use_percentaje_exp"></div>
-  <br>
-  <div v-katex="build_cost_exp"></div>
-  <br>
-  USO
-  <div v-katex="use_cost_exp"></div>
-  FIN DE CICLO DE VIDA
-  <div v-katex="end_life_exp"></div>
-  <div v-katex="test()"></div>
+  <div v-for=" (use) in uses" :key="use.pk" style="margin-bottom: 50px">
+    <h1>{{ use.component.name }}</h1>
+    <h3>FABRICACION</h3>
+    <div v-katex="use_percentaje_exp"></div>
+    <div v-katex="build_cost_exp"></div>
+
+    <div v-katex="use_percentaje(use)"></div>
+
+    <div v-katex="build_cost(use, 'middle')"></div>
+    <div v-katex="build_cost(use, 'worst')"></div>
+    <div v-katex="build_cost(use, 'best')"></div>
+
+    <h3>USO</h3>
+    <div v-katex="use_cost_exp"></div>
+
+    <h3>FIN DE CICLO DE VIDA</h3>
+    <div v-katex="end_life_exp"></div>
+    <div v-katex="destruction_cost(use, 'middle')"></div>
+    <div v-katex="destruction_cost(use, 'worst')"></div>
+    <div v-katex="destruction_cost(use, 'best')"></div>
+  </div>
+
 
 </template>
 
@@ -40,13 +52,6 @@ export default {
     }
   },
   methods: {
-    calculat2e(){
-      console.log("hola")
-      for (const i in this.uses){
-        this.uses[i]["use_percent"] = (this.uses[i].hours / this.useful_hours.number) * 100;
-        this.uses[i]["build_cost"] = 2;
-      }
-    },
     getComponent(id){
       let component = this.store.components.system.filter((item) => item.id === id);
       
@@ -84,13 +89,66 @@ export default {
         use["destroy_cost_bc"] = parseFloat(destroy_cost_bc.toFixed(2));
         use["total_bc"] = use["destroy_cost_bc"] + use["use_cost_bc"] + use["build_cost_bc"];        
 
+        use["component"] = component;
         this.uses[i] = use;
       }
     },
-    test() {
-      console.log("test")
-      console.log(this.uses)
-      return "\\textbf{Coste destrucción}= (\\text{"+ this.uses[0]["build_cost"] +"} +  \\text{"+ this.uses[0]["use_cost"] +"})\\cdot 0.01"
+    use_percentaje(use) {
+      return "\\textbf{Porcentaje de uso} = \\frac{\\text{" + use.hours +"}}{\\text{" + this.useful_hours.string +"}}\\cdot 100 = "+ use.use_percent;
+    },
+    build_cost(use, case_type) {
+      var cfp_build_phase = "";
+      var use_percent = "";
+      var build_cost = "";
+      var name = ""
+
+
+      use_percent = use.use_percent;
+      switch (case_type) {
+        case 'middle':
+          name = " (caso medio)"
+          cfp_build_phase = use.component.cfp_build_phase;
+          build_cost = use.build_cost;
+          break;
+        case 'worst':
+          name = " (peor caso)"
+          cfp_build_phase = use.component.cfp_build_phase + use.component.cfp_deviation_standard;
+          build_cost = use.build_cost_wc;
+          break;
+        case 'best':
+          name = " (mejor caso)"
+          cfp_build_phase = use.component.cfp_build_phase - use.component.cfp_deviation_standard;
+          build_cost = use.build_cost_bc;
+          break;
+      }
+      return "\\textbf{Coste fabricacion en el proyecto "+ name +"}=\\text{"+ cfp_build_phase +"}\\cdot \\text{"+ use_percent +"} = "+ build_cost;
+    },
+    destruction_cost(use, case_type) {
+      var build_cost = "";
+      var use_cost = "";
+      var destroy_cost = "";
+      var name = ""
+      switch (case_type) {
+        case 'middle':
+          name = " (caso medio)"
+          build_cost = use.build_cost;
+          use_cost = use.use_cost;
+          destroy_cost = use.destroy_cost;
+          break;
+        case 'worst':
+          name = " (peor caso)"
+          build_cost = use.build_cost_wc;
+          use_cost = use.use_cost_wc;
+          destroy_cost = use.destroy_cost_wc;
+          break;
+        case 'best':
+          name = " (mejor caso)"
+          build_cost = use.build_cost_bc;
+          use_cost = use.use_cost_bc;
+          destroy_cost = use.destroy_cost_bc;
+          break;
+      }
+      return "\\textbf{Coste destrucción" + name + "}= (\\text{" + build_cost + "} +  \\text{"+ use_cost +"})\\cdot 0.01 = " + destroy_cost;
     }
   },
   created(){
